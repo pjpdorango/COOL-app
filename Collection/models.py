d50bfee8225138155f85edf431090260a83a97bf
PJ <b2026pjpdorango@pshs.edu.ph>
2024-06-28 02:35:57 +0800

    Created Collection class in models.py

--- Collection/models.py
+++ Collection/models.py
@@ -1,3 +1,14 @@
+from django.db.models import *
 from django.db import models
 
+# from ..Subject.models import 
+
 # Create your models here.
+class Collection(Model):
+    name: CharField = CharField(
+        max_length=25,
+        blank=False,
+        null=False
+    )
+
+    visibility: models.DateField
